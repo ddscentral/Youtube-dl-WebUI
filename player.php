1f412797dc19dad295360f65203cb93c4934bff8
ddscentral <dds@ddscentral.org>
2022-10-18 13:56:38 +0300

    Remove relative path to video from player page title.

--- player.php
+++ player.php
@@ -39,7 +39,7 @@
 <html>
         <head>
                 <meta charset="utf-8">
-                <title>Youtube-dl Player - <?php echo htmlspecialchars($fileName); ?></title>
+                <title>Youtube-dl Player - <?php echo htmlspecialchars(basename($fileName)); ?></title>
                 <!-- https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css -O cdn/bootstrap.min.css -->
                 <link rel="stylesheet" href="./css/cdn/bootstrap.min.css" media="screen">
                 <link rel="stylesheet" href="./css/custom.css">
